52f74906a85ff643d05b7ea6d1e3c2cf4eee22a1
ricardo <ricardo@enmotionvalue.com>
2017-06-28 01:11:46 +0200

    borrado TasksProjects

--- TasksProjects/src/app/core/api.service.ts
+++
@@ -1,9 +0,0 @@
-import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs/Rx';
-
-@Injectable()
-export class ApiService{
-	sayHello(){
-		return Observable.of('hello world');
-	}
-}
